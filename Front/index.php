d23d7b314c702b91ade4a6f57d4cc8166f9f64e2
Léonard <leonard@agence-me.com>
2020-01-21 14:28:44 +0100

    Add - Front - Home page

--- Front/index.php
+++ Front/index.php
@@ -15,13 +15,201 @@
 	
 	<body>
 
-		<?php include('common-header.php') ?>
-
 		<main>
+
+			<header>
+				<img class="logo" src="img/common/logo.png" alt="">
+				<div class="container-link">
+					<a href="#anchor-section-features">Notre mission</a>
+					<a href="#anchor-section-team">Notre équipe</a>
+				</div>
+				<a href="tel:0681498185" class="btn">
+					<span class="btn-text">
+						06 81 49 81 85
+					</span>
+				</a>
+			</header>
+
+			<section class="section-cover">
+				<div class="background"></div>
+				<div class="wrapper">
+					<h1>
+						Nous aurons la meilleure proposition pour votre projet immoblier
+					</h1>
+					<div class="container-mail">
+						<form action="">
+							<input type="text" placeholder="Laissez votre adresse mail">
+							<button>
+								<span class="btn-text">CONTACTEZ-NOUS</span>
+							</button>
+						</form>
+					</div>
+				</div>
+			</section>
+
+			<section id="anchor-section-features" class="section-features">
+				<div class="wrapper">
+
+					<div class="container-title">
+						<h2>
+							Pourquoi nous faire <b>confiance ?</b>
+						</h2>
+					</div>
+
+					<div class="container-el">
+					
+						<div class="el">
+							<div class="text">
+								<h3>Lorem ipsum dolor</h3>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus arcu ante, a luctus dolor dignissim id. Quisque dapibus egestas risus id malesuada. 
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis. Nunc felis diam, ultricies ac lacus nec, ornare euismod quam. 
+								</p>
+							</div>
+							<div class="img">
+								<img src="img/img-test/background-1.jpg" alt="">
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="img">
+								<img src="img/img-test/background-1.jpg" alt="">
+							</div>
+							<div class="text">
+								<h3>Lorem ipsum dolor</h3>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus arcu ante, a luctus dolor dignissim id. Quisque dapibus egestas risus id malesuada. 
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis. Nunc felis diam, ultricies ac lacus nec, ornare euismod quam. 
+								</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+
+			<section id="anchor-section-team" class="section-team">
+				<div class="background"></div>
+				<div class="wrapper">
+					<div class="container-title">
+						<h2>
+							Qui sommes-nous ? Rencontrez<br><b>notre équipe<br>d’experts</b>
+						</h2>
+						<div class="desc">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus arcu ante, a luctus dolor dignissim id.  
+							</p>
+							<a href="">
+								<span class="btn-text">CONTACTEZ-NOUS</span>
+							</a>
+						</div>
+					</div>
+					<div class="container-team">
+						<div class="team">
+							<img src="img/img-test/user.jpg" alt="">
+							<div class="text">
+								<div class="name">Michel Legrand</div>
+								<p>
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis.
+								</p>
+							</div>
+						</div>
+						<div class="team">
+							<img src="img/img-test/user.jpg" alt="">
+							<div class="text">
+								<div class="name">Michel Legrand</div>
+								<p>
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis.
+								</p>
+							</div>
+						</div>
+						<div class="team">
+							<img src="img/img-test/user.jpg" alt="">
+							<div class="text">
+								<div class="name">Michel Legrand</div>
+								<p>
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis.
+								</p>
+							</div>
+						</div>
+						<div class="team">
+							<img src="img/img-test/user.jpg" alt="">
+							<div class="text">
+								<div class="name">Michel Legrand</div>
+								<p>
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis.
+								</p>
+							</div>
+						</div>
+						<div class="team">
+							<img src="img/img-test/user.jpg" alt="">
+							<div class="text">
+								<div class="name">Michel Legrand</div>
+								<p>
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis.
+								</p>
+							</div>
+						</div>
+						<div class="team">
+							<img src="img/img-test/user.jpg" alt="">
+							<div class="text">
+								<div class="name">Michel Legrand</div>
+								<p>
+									Sed nisl lorem, luctus vel vestibulum a, placerat dictum mi. Nullam euismod velit id leo aliquet lobortis.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-place">
+				<div class="wrapper">
+					<div class="container-title">
+						<h2>
+							Ou sommes- nous ?
+						</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus arcu ante, a luctus dolor dignissim id. Quisque dapibus egestas risus id malesuada. 
+						</p>
+					</div>
+					<div class="container-places">
+						<div class="place">
+							<div class="title">CREDEXIMO GRAND NORD</div>
+							<p>
+								23 rue du grand palais
+								59000 Lille cedex
+							</p>
+						</div>
+						<div class="place">
+							<div class="title">CREDEXIMO GRAND NORD</div>
+							<p>
+								23 rue du grand palais
+								59000 Lille cedex
+							</p>
+						</div>
+						<div class="place">
+							<div class="title">CREDEXIMO GRAND NORD</div>
+							<p>
+								23 rue du grand palais
+								59000 Lille cedex
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<footer>
+
+				<img class="logo" src="img/common/logo-white.png" alt="">
+
+				<div class="foot">
+					<p>©2019Credeximo. All rights Reserved</p>
+				</div>
+				
+			</footer>
 			
 		</main>
-
-		<?php include('common-footer.php') ?>
 
 		<script type="text/javascript" src="script/minify/index-min.js"></script>
 	</body>
